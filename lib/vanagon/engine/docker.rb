e84135bab373b5a3c4ada52f29a90c034f481d5b
Matthaus Owens <matthaus@puppetlabs.com>
2015-02-24 15:54:24 -0800

    (RE-3822) Add support for build engines to vanagon
    Previously target selection, setup and teardown was written into
    driver.rb, with some conditionals to decide which path to take. This
    commit tries to make this situation better by adding the concept of an
    engine to vanagon. The engine will define how to select a build target,
    how to set it up as a builder, and how to tear it down when done. The
    initial work includes a base engine which supports ssh, and a docker and
    pooler backend.

---
+++ lib/vanagon/engine/docker.rb
@@ -0,0 +1,40 @@
+require 'vanagon/engine/base'
+
+class Vanagon
+  class Engine
+    class Docker < Base
+      # Both the docker_image and the docker command itself are required for
+      # the docker engine to work
+      def initialize(platform, target = nil)
+        @docker_cmd = Vanagon::Utilities.find_program_on_path('docker')
+        super
+        @required_attributes << "docker_image"
+      end
+
+      # This method is used to obtain a vm to build upon using
+      # a docker container.
+      # @raise [Vanagon::Error] if a target cannot be obtained
+      def select_target
+        Vanagon::Utilities.ex("#{@docker_cmd} run -d --name #{@platform.docker_image}-builder -p #{@platform.ssh_port}:22 #{@platform.docker_image}")
+        @target = 'localhost'
+
+        # Wait for ssh to come up in the container
+        Vanagon::Utilities.retry_with_timeout do
+          Vanagon::Utilities.remote_ssh_command("#{@target_user}@#{@target}", 'exit', @platform.ssh_port)
+        end
+      rescue => e
+        raise Vanagon::Error.wrap(e, "Something went wrong getting a target vm to build on using docker. Ssh was not up in the container after 5 seconds.")
+      end
+
+      # This method is used to tell the vmpooler to delete the instance of the
+      # vm that was being used so the pool can be replenished.
+      def teardown
+        Vanagon::Utilities.ex("#{@docker_cmd} stop #{@platform.docker_image}-builder")
+        Vanagon::Utilities.ex("#{@docker_cmd} rm #{@platform.docker_image}-builder")
+      rescue RuntimeError => e
+        warn "There was a problem tearing down the docker container #{@platform.docker_image}-builder (#{e.message})."
+      end
+    end
+  end
+end
+
---
+++ lib/vanagon/engine/pooler.rb
@@ -0,0 +1,43 @@
+require 'vanagon/engine/base'
+
+class Vanagon
+  class Engine
+    class Pooler < Base
+      # The vcloud_name is required to use the pooler engine
+      def initialize(platform, target = nil)
+        @pooler = "http://vmpooler.delivery.puppetlabs.net"
+        super
+        @required_attributes << "vcloud_name"
+      end
+
+      # This method is used to obtain a vm to build upon using the Puppet Labs'
+      # vmpooler (https://github.com/puppetlabs/vmpooler)
+      # @raise [Vanagon::Error] if a target cannot be obtained
+      def select_target
+        response = Vanagon::Utilities.http_request("#{@pooler}/vm/#{@platform.vcloud_name}", "POST")
+        if response and response["ok"]
+          Vanagon::Driver.logger.info "Reserving #{response[@platform.vcloud_name]['hostname']} (#{@platform.vcloud_name})"
+          @target = response[@platform.vcloud_name]['hostname']
+        else
+          raise Vanagon::Error.new("Something went wrong getting a target vm to build on, maybe the pool for #{@platform.vcloud_name} is empty?")
+        end
+      end
+
+      # This method is used to tell the vmpooler to delete the instance of the
+      # vm that was being used so the pool can be replenished.
+      def teardown
+        response = Vanagon::Utilities.http_request("#{@pooler}/vm/#{@target}", "DELETE")
+        if response and response["ok"]
+          Vanagon::Driver.logger.info "#{@target} has been destroyed"
+          puts "#{@target} has been destroyed"
+        else
+          Vanagon::Driver.logger.info "#{@target} could not be destroyed"
+          warn "#{@target} could not be destroyed"
+        end
+      rescue Vanagon::Error => e
+        Vanagon::Driver.logger.info "#{@target} could not be destroyed (#{e.message})"
+        warn "#{@target} could not be destroyed (#{e.message})"
+      end
+    end
+  end
+end
