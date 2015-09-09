595c936f1c89bb361ce1239109780132c037391e
Scott Schneider <sschneider@puppetlabs.com>
2015-09-08 22:33:14 -0700

    (QENG-2928) Add token support to vmpooler requests
    Now that http_request supports adding headers, this commit adds a token
    read from either an environment variable or file to the http request for
    pooler vm requests, to ensure that they get the full 12 hour lifetime
    and are correctly tagged.

--- lib/vanagon/engine/pooler.rb
+++ lib/vanagon/engine/pooler.rb
@@ -3,11 +3,28 @@ require 'vanagon/engine/base'
 class Vanagon
   class Engine
     class Pooler < Base
+      attr_reader :token
+
       # The vcloud_name is required to use the pooler engine
       def initialize(platform, target = nil)
         @pooler = "http://vmpooler.delivery.puppetlabs.net"
+        @token = load_token
         super
         @required_attributes << "vcloud_name"
+      end
+
+      # This method loads the pooler token from one of two locations
+      # @return [String, nil] token for use with the vmpooler
+      def load_token
+        if ENV['VMPOOLER_TOKEN']
+          token = ENV['VMPOOLER_TOKEN']
+        else
+          token_file = File.expand_path("~/.vanagon-token")
+          if File.exist?(token_file)
+            token = File.open(token_file).read.chomp
+          end
+        end
+        token
       end
 
       # This method is used to obtain a vm to build upon using the Puppet Labs'
@@ -17,11 +34,12 @@ class Vanagon
         response = Vanagon::Utilities.http_request(
           "#{@pooler}/vm",
           'POST',
-          '{"' + @platform.vcloud_name + '":"1"}'
+          '{"' + @platform.vcloud_name + '":"1"}',
+          { 'X-AUTH-TOKEN' => @token }
         )
         if response and response["ok"]
           @target = response[@platform.vcloud_name]['hostname'] + '.' + response['domain']
-          Vanagon::Driver.logger.info "Reserving #{@target} (#{@platform.vcloud_name})"
+          Vanagon::Driver.logger.info "Reserving #{@target} (#{@platform.vcloud_name}) [#{@token ? 'token used' : 'no token used'}]"
 
           tags = {
             'tags' => {
@@ -34,7 +52,8 @@ class Vanagon
           response_tag = Vanagon::Utilities.http_request(
             "#{@pooler}/vm/#{response[@platform.vcloud_name]['hostname']}",
             'PUT',
-            tags.to_json
+            tags.to_json,
+            { 'X-AUTH-TOKEN' => @token }
           )
         else
           raise Vanagon::Error, "Something went wrong getting a target vm to build on, maybe the pool for #{@platform.vcloud_name} is empty?"
@@ -44,7 +63,12 @@ class Vanagon
       # This method is used to tell the vmpooler to delete the instance of the
       # vm that was being used so the pool can be replenished.
       def teardown
-        response = Vanagon::Utilities.http_request("#{@pooler}/vm/#{@target}", "DELETE")
+        response = Vanagon::Utilities.http_request(
+          "#{@pooler}/vm/#{@target}",
+          "DELETE",
+          nil,
+          { 'X-AUTH-TOKEN' => @token }
+        )
         if response and response["ok"]
           Vanagon::Driver.logger.info "#{@target} has been destroyed"
           puts "#{@target} has been destroyed"
--- spec/lib/vanagon/engine/pooler_spec.rb
+++ spec/lib/vanagon/engine/pooler_spec.rb
@@ -1,6 +1,7 @@
 require 'vanagon/engine/pooler'
 
 describe 'Vanagon::Engine::Pooler' do
+  let (:platform) { double(Vanagon::Platform) }
   let (:platform_with_vcloud_name) {
     plat = Vanagon::Platform::DSL.new('debian-6-i386')
     plat.instance_eval("platform 'debian-6-i386' do |plat|
@@ -15,6 +16,31 @@ describe 'Vanagon::Engine::Pooler' do
                     end")
     plat._platform
   }
+
+  describe "#load_token" do
+    after(:each) { ENV['VMPOOLER_TOKEN'] = nil }
+
+    let(:token_file) { double(File) }
+    let(:token_filename) { 'abcd' }
+
+    it 'prefers an env var to a file' do
+      ENV['VMPOOLER_TOKEN'] = 'abcd'
+      expect(File).to_not receive(:expand_path).with('~/.vanagon-token')
+      expect(Vanagon::Engine::Pooler.new(platform).token).to eq('abcd')
+    end
+
+    it 'falls back to a file if the env var is not set' do
+      expect(File).to receive(:expand_path).with('~/.vanagon-token').and_return(token_filename)
+      expect(File).to receive(:exist?).with(token_filename).and_return(true)
+      expect(File).to receive(:open).with(token_filename).and_return(token_file)
+      expect(token_file).to receive(:read).and_return('abcd')
+      expect(Vanagon::Engine::Pooler.new(platform).token).to eq('abcd')
+    end
+
+    it 'returns nil if there is no env var or file' do
+      expect(Vanagon::Engine::Pooler.new(platform).token).to be_nil
+    end
+  end
 
   describe "#validate_platform" do
     it 'raises an error if the platform is missing a required attribute' do
