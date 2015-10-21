9e870f267996787c25d327ea436145c0c5643ef6
Melissa Stone <melissa@puppetlabs.com>
2015-10-21 11:28:07 -0700

    (maint) Teach vanagon how to handle xml sources

--- lib/vanagon/component/source/http.rb
+++ lib/vanagon/component/source/http.rb
@@ -13,7 +13,7 @@ class Vanagon
         ARCHIVE_EXTENSIONS = '.tar.gz', '.tgz'
 
         # Extensions for files we aren't going to unpack during the build
-        NON_ARCHIVE_EXTENSIONS = '.gem', '.ru', '.txt', '.conf', '.ini', '.gpg', '.rb', '.sh', '.csh'
+        NON_ARCHIVE_EXTENSIONS = '.gem', '.ru', '.txt', '.conf', '.ini', '.gpg', '.rb', '.sh', '.csh', '.xml'
 
         # Constructor for the Http source type
         #
