f0238adec7d365fd1d5558102c6816cc9d0647bb
Melissa Stone <melissa@puppetlabs.com>
2015-12-31 00:54:08 +0000

    (maint) Specify install command for windows
    We want to default to cygwin install on windows

--- lib/vanagon/platform/windows.rb
+++ lib/vanagon/platform/windows.rb
@@ -194,6 +194,7 @@ class Vanagon
         @find = "/usr/bin/find"
         @sort = "/usr/bin/sort"
         @num_cores = "/usr/bin/nproc"
+        @install = "/usr/bin/install"
         @copy = "/usr/bin/cp"
         super(name)
       end
