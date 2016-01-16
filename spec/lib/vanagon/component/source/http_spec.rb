e2958111d3dd8c508b415f05dae98b142dab5463
Michael Stahnke <stahnma@puppetlabs.com>
2016-01-16 06:29:38 +0000

    Add upstream URL to component sources
    When trying to work with puppet-agent and other PL software outside of
    Puppet Labs, things are difficult. This is an attempt to make it
    slightly easier. This commit adds a new DSL method called 'upstream_url'
    to a component source. This URL is used for fetching content if the
    'url' specified fails.
    
    The advantage of this is that we'll now have the upstream URL in the
    component, and you won't have to spend your first 25 minutes download
    sources and making a mirror on a webserver to use vanagon's url rewrite
    engine.
    
    Basically, this just means you need something like:
    
        pkg.upstream_url "http://ftp.gnu.org/gnu/#{pkg.get_name}/#{pkg.get_name}-#{pkg.get_version}.tar.gz"
    
    Upstream URL is always the second choice behind pkg.url.
    
    This patch has no bearing on handling of git urls.

--- spec/lib/vanagon/component/source/http_spec.rb
+++ spec/lib/vanagon/component/source/http_spec.rb
@@ -11,17 +11,18 @@ describe "Vanagon::Component::Source::Http" do
   let (:plaintext_dirname) { './' }
   let (:md5sum) { 'abcdssasasa' }
   let (:workdir) { "/tmp" }
+  let (:upstream_url) { "http://ftp.gnu.org/#{tar_dirname}/#{tar_filename}" }
 
   describe "#dirname" do
     it "returns the name of the tarball, minus extension for archives" do
-      http_source = Vanagon::Component::Source::Http.new(tar_url, md5sum, workdir)
+      http_source = Vanagon::Component::Source::Http.new(tar_url, md5sum, workdir, upstream_url)
       expect(http_source).to receive(:download).and_return(tar_filename)
       http_source.fetch
       expect(http_source.dirname).to eq(tar_dirname)
     end
 
     it "returns the current directory for non-archive files" do
-      http_source = Vanagon::Component::Source::Http.new(plaintext_url, md5sum, workdir)
+      http_source = Vanagon::Component::Source::Http.new(plaintext_url, md5sum, workdir, upstream_url)
       expect(http_source).to receive(:download).and_return(plaintext_filename)
       http_source.fetch
       expect(http_source.dirname).to eq(plaintext_dirname)
@@ -33,7 +34,7 @@ describe "Vanagon::Component::Source::Http" do
       Vanagon::Component::Source::Http::ARCHIVE_EXTENSIONS.each do |ext|
         filename = "#{file_base}#{ext}"
         url = File.join(base_url, filename)
-        http_source = Vanagon::Component::Source::Http.new(url, md5sum, workdir)
+        http_source = Vanagon::Component::Source::Http.new(url, md5sum, workdir, upstream_url)
         expect(http_source).to receive(:download).and_return(filename)
         http_source.fetch
         expect(http_source.get_extension).to eq(ext)
@@ -43,7 +44,7 @@ describe "Vanagon::Component::Source::Http" do
     it "is able to download non archive extensions" do
       ["gpg.txt", "foo.service", "configi.json", "config.repo.txt", "noextensionfile"].each do |filename|
         url = File.join(base_url, filename)
-        http_source = Vanagon::Component::Source::Http.new(url, md5sum, workdir)
+        http_source = Vanagon::Component::Source::Http.new(url, md5sum, workdir, upstream_url)
         expect(http_source).to receive(:download).and_return(filename)
         http_source.fetch
       end
