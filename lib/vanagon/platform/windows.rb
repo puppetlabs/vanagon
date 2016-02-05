1b33dfbed5669b3f9fd872af0817d08e859115e1
John O'Connor <johnoconnor@Johns-MacBook-Pro-3.local>
2016-02-05 14:40:17 +0000

    (RE-6468) Compile the list of file components
    Use candle to compile the list of files into a single wixobj file
    All .wxs files under the wix directory are compiled into wixobj files in
    the wixobj directory

--- lib/vanagon/platform/windows.rb
+++ lib/vanagon/platform/windows.rb
@@ -107,9 +107,10 @@ class Vanagon
         target_dir = project.repo ? output_dir(project.repo) : output_dir
         cg_name = "ProductComponentGroup"
         dir_ref = "INSTALLDIR"
+        candle_flags =  "-dPlatform=#{@architecture} -arch #{@architecture} -ext WiXUtilExtension -ext WixUIExtension"
         # Actual array of commands to be written to the Makefile
         ["mkdir -p output/#{target_dir}",
-          "mkdir -p $(tempdir)/{staging,wix}",
+          "mkdir -p $(tempdir)/{staging,wix/wixobj}",
           "#{@copy} -r wix/* $(tempdir)/wix/",
           "gunzip -c #{project.name}-#{project.version}.tar.gz | '#{@tar}' -C '$(tempdir)/staging' --strip-components 1 -xf -",
           # Run the Heat command in a single pass
@@ -121,6 +122,9 @@ class Vanagon
           #   -dr             - Directory reference to root directories (cannot contains spaces e.g. -dr MyAppDirRef)
           #   -sreg           - Suppress registry harvesting.
           "cd $(tempdir); \"$$WIX/bin/heat.exe\" dir staging -v -ke -indent 2 -cg #{cg_name} -gg -dr #{dir_ref} -t wix/#{project.name}.filter.xslt -sreg -out wix/#{project.name}-harvest.wxs",
+          # Apply Candle command to all *.wxs files - generates .wixobj files in wix directory.
+          # cygpath conversion is necessary as candle is unable to handle posix path specs
+          "cd $(tempdir)/wix/wixobj; for wix_file in `find $(tempdir)/wix -name \'*.wxs\'`; do \"$$WIX/bin/candle.exe\" #{candle_flags} $$(cygpath -aw $$wix_file) ; done",
           ]
       end
 
