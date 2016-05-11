puppetlabs/vanagon

commit bab55a46ff25f1406da91467b5d8ef9266c78d92

Add Amazon Ec2 Engine support.

lib/vanagon/engine/ec2.rb
@@ -1,0 +1,71 @@
+require 'aws-sdk'
+require 'erb'
+require 'base64'
+require 'vanagon/engine/base'
+
+class Vanagon
+  class Engine
+    class Ec2 < Base
+      attr_accessor :ami, :key_name, :userdata, :key, :key_name, :shutdown_behavior
+      attr_accessor :subnet_id, :instance_type,
+
+      def initialize(platform, target = nil)
+        super
+        @name = 'ec2'
+        @platform = platform
+        @ami = @platform.aws_ami
+        @target_user = @platform.target_user
+        @subnet_id = @platform.aws_subnet_id
+        @userdata = @platform.aws_user_data
+        @region = @platform.aws_region || 'us-east-1'
+        @key_name = @platform.aws_key_name
+        @key = @platform.aws_key
+        @instance_type = @platform.aws_instance_type || "t1.micro"
+        @required_attributes = ["ssh_port", "aws_ami", "aws_key_name"]
+        @shutdown_behavior = @platform.aws_shutdown_behavior
+
+        @ec2 = ::Aws::EC2::Client.new(region: @region)
+        @resource = ::Aws::EC2::Resource.new(client: @ec2)
+      end
+
+      def get_userdata()
+        unless @userdata.nil?
+          Base64.encode64(ERB.new(@userdata).result(binding))
+        end
+      end
+
+      def instances
+        @instances ||= @resource.create_instances({
+          image_id: ami,
+          min_count: 1,
+          max_count: 1,
+          key_name: key_name,
+          instance_type: instance_type,
+          subnet_id: subnet_id,
+          user_data: get_userdata,
+          monitoring: {
+            enabled: false,
+          }
+        })
+      end
+
+      def instance
+        @instance ||= instances.first
+      end
+
+      def select_target
+        puts "Created instance waiting for it to start up"
+        @ec2.wait_until(:instance_status_ok, instance_ids: [@instance[:id]])
+        puts "Instance created id: #{@instance[:id]}"
+        @target = @instance[:private_ip_address]
+      rescue ::Aws::Waiters::Errors::WaiterFailed => error
+        fail "Failed to wait for ec2 instance to start got error #{error}"
+      end
+
+      def teardown
+        puts "Destroying instance on AWS id: #{@instances.map(&:id).first}"
+        @instances.batch_terminate!
+      end
+    end
+  end
+end

Gemfile
@@ -10,6 +10,8 @@ def lock_manager_location_for(place)
   end
 end
 
+gem "aws-sdk", "~> 2.2.0"
+
 group(:development, :test) do
   gem 'rspec', '~> 3.0', :require => false
   gem 'yard', :require => false

lib/vanagon/driver.rb
@@ -36,6 +36,10 @@ class Vanagon
     def load_engine(engine_type, platform, target)
       if platform.build_hosts
         engine_type = 'hardware'
+      elsif platform.aws_ami
+        engine_type = 'ec2'
+      elsif platform.docker_image
+        engine_type = 'docker'
       elsif target
         engine_type = 'base'
       end
@@ -106,7 +110,7 @@ class Vanagon
       puts e.backtrace.join("\n")
       raise e
     ensure
-      if @engine.name == "hardware"
+      if ["hardware", "ec2"].include?(@engine.name)
         @engine.teardown
       end
     end

lib/vanagon/platform.rb
@@ -7,6 +7,8 @@ class Vanagon
     attr_accessor :servicetype, :patch, :architecture, :codename, :os_name, :os_version
     attr_accessor :docker_image, :ssh_port, :rpmbuild, :install, :platform_triple
     attr_accessor :target_user, :package_type, :find, :sort, :build_hosts, :copy, :cross_compiled
+    attr_accessor :aws_ami, :aws_user_data, :aws_shutdown_behavior, :aws_key_name, :aws_region, :aws_key
+    attr_accessor :aws_instance_type, :aws_vpc_id, :aws_subnet_id
 
     # Platform names currently contain some information about the platform. Fields
     # within the name are delimited by the '-' character, and this regex can be used to

lib/vanagon/platform/dsl.rb
@@ -228,11 +228,74 @@ class Vanagon
         @platform.docker_image = name
       end
 
+      # Set the ami for the platform to use
+      #
+      # @param ami [String] the ami id used.
+      def aws_ami(ami)
+        @platform.aws_ami = ami
+      end
+
+      # Set the user data used in AWS to do setup. Like cloud-config
+      #
+      # @param userdata [String] a string used to send to the node to do the intial setup
+      def aws_user_data(userdata)
+        @platform.aws_user_data = userdata
+      end
+
+      # Set the region, this defaults to us-east-1
+      #
+      # @param region [String] a string used to setup the region
+      def aws_region(region = 'us-east-1')
+        @platform.aws_region = region
+      end
+
+      # Set the shutdown behavior for aws. This will default to terminate the instance on shutdown
+      #
+      # @param shutdown_behavior [String] a string used to set the shutdown behavior
+      def aws_shutdown_behavior(shutdown_behavior = 'terminate')
+        @platform.aws_shutdown_behavior = shutdown_behavior
+      end
+
+      # Set the key_name used. This should already exist on AWS.
+      #
+      # @param key_name [String] this defaults to the keyname vanagon. Can be set to any
+      def aws_key_name(key_name = 'vanagon')
+        @platform.aws_key_name = key_name
+      end
+
+      # Set the instaince type. This defaults to t1.micro which is the free instance
+      #
+      # @param instance_type [String] a string to define the instaince type
+      def aws_instance_type(instance_type = 't1.micro')
+        @platform.aws_instance_type = instance_type
+      end
+
+      # Set the subnet_id. Use this to setup a subnet for your VPC to use.
+      #
+      # @param subnet_id[String] a string to define the subnet_id in AWS
+      def aws_subnet_id(subnet_id)
+        @platform.aws_subnet_id = subnet_id
+      end
+
       # Set the port for ssh to use if it's not 22
       #
       # @param port [Integer] port number for ssh
       def ssh_port(port = 22)
         @platform.ssh_port = port
+      end
+
+      # Set the target user to login with. Defaults to root.
+      #
+      # @param user[String] a user string to login with.
+      def target_user(user = "root")
+        @platform.target_user = user
+      end
+
+      # Set the target ip address or hostname to start build
+      #
+      # @param target_host[String] a host string to login with.
+      def target_host(target_host)
+        @platform.target_host = target_host
       end
 
       # Set the platform_triple for the platform

commit 32ea41954b725f32585266869e28fc53ab6c807c

(maint) Add unit test for ec2 engine
Commit bab55a46 had a trailing comma after `:instance_type`, which seems
to cause the following `initialize` method to be ignored. So trying to
create an EC2 engine and passing in a platform didn't work:

    irb(main):033:0> Vanagon::Engine::Ec2.new(plat._platform)
    ArgumentError: wrong number of arguments (1 for 0)

Commit b8f537a74 changed how engines return their name, so it's no
longer an instance variable. That combined with bab55a46, caused the
Ec2 engine to report its name as 'base':

    irb(main):035:0> Vanagon::Engine::Ec2.new.name
    => "base"

This commit removes the trailing comma, fixes the engine name, and adds
a unit test to make sure we can create an EC2 engine and get the correct
name.

lib/vanagon/engine/ec2.rb
@@ -7,12 +7,11 @@ class Vanagon
   class Engine
     class Ec2 < Base
       attr_accessor :ami, :key_name, :userdata, :key, :key_name, :shutdown_behavior
-      attr_accessor :subnet_id, :instance_type,
+      attr_accessor :subnet_id, :instance_type
 
       def initialize(platform, target = nil)
         super
-        @name = 'ec2'
-        @platform = platform
+
         @ami = @platform.aws_ami
         @target_user = @platform.target_user
         @subnet_id = @platform.aws_subnet_id
@@ -28,7 +27,11 @@ class Vanagon
         @resource = ::Aws::EC2::Resource.new(client: @ec2)
       end
 
-      def get_userdata()
+      def name
+        'ec2'
+      end
+
+      def get_userdata
         unless @userdata.nil?
           Base64.encode64(ERB.new(@userdata).result(binding))
         end

spec/lib/vanagon/engine/ec2_spec.rb
@@ -1,0 +1,25 @@
+require 'vanagon/engine/ec2'
+require 'vanagon/platform'
+
+describe 'Vanagon::Engine::Ec2' do
+  let(:platform_ec2) do
+    plat = Vanagon::Platform::DSL.new('el-7-x86_64')
+    plat.instance_eval(<<-END)
+      platform 'el-7-x86_64' do |plat|
+        plat.aws_ami 'ami'
+        plat.target_user 'root'
+        plat.aws_subnet_id 'subnet_id'
+        plat.aws_user_data 'user_data'
+        plat.aws_region 'us-west-1'
+        plat.aws_key_name 'vanagon'
+        plat.aws_instance_type 't1.micro'
+        plat.ssh_port '22'
+      end
+    END
+    plat._platform
+  end
+
+  it 'returns "ec2" name' do
+    expect(Vanagon::Engine::Ec2.new(platform_ec2).name).to eq('ec2')
+  end
+end