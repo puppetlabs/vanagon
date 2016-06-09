66b058053e865fffbd3e940f4b9c217d8c737cbb
Michael Stahnke <stahnma@puppetlabs.com>
2016-06-09 22:43:37 +0000

    (maint) Increase default timeout
    When building toolchain components such as GCC, it takes more than 1
    hours. This ups the default timeout to 2 hours since our jenkins configs
    don't pass timeout as a parameter.

--- lib/vanagon/driver.rb
+++ lib/vanagon/driver.rb
@@ -138,7 +138,7 @@ class Vanagon
     # values from the project, if available, otherwise use some
     # sane defaults.
     def retry_task(&block)
-      @timeout = @project.timeout || ENV["TIMEOUT"] || 3600
+      @timeout = @project.timeout || ENV["TIMEOUT"] || 7200
       @retry_count = @project.retry_count || ENV["RETRY_COUNT"] || 1
       Vanagon::Utilities.retry_with_timeout(@retry_count, @timeout) { yield }
     end
