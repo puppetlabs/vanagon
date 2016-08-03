4e110c4acc4f55f44a4c3d9a023a47694798c040
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-08-02 22:26:17 -0700

    (MAINT) Add doc for Git library extension
    Added a little yardoc to the extension for the Ruby-Git library.

--- lib/git/basic_submodules.rb
+++ lib/git/basic_submodules.rb
@@ -18,6 +18,23 @@ module BasicSubmodulePrimitives
 end
 
 module BasicSubmodules
+  # @example Initialize all git submodules
+  #   >> repo = Git.clone("git@github.com:puppetlabs/facter.git", "facter", path: Dir.mktmpdir)
+  #   => <Git::Base>
+  #   >> repo.checkout "3.1.3
+  #   => <String>
+  #   >> repo.update_submodules(init: true)
+  #   => <String>
+  # @param [Hash] options any options to pass to `git submodule update`
+  # @option options [Boolean] :init whether to initialize submodules when updating them
+  # @option options [Boolean] :use the submodule's remote-tracking branch instead of superproject's SHA1 sum
+  # @option options [Boolean] :no-fetch don't fetch new objects from the remote site.
+  # @option options [Boolean] :force remove submodule's working tree even if modified
+  # @option options [Boolean] :checkout checkout submodules in detached HEAD state
+  # @option options [Boolean] :merge merge recorded commit for submodule into the current branch of the submodule
+  # @option options [Boolean] :rebase rebase current branch of submodule onto the commit recorded in the superproject
+  # @option options [Boolean] :recursive recurse into nested submodules
+  # @return options [String] any output produced by `git` when submodules are initialized
   def update_submodules(**options)
     self.lib.update_submodules(options)
   end
