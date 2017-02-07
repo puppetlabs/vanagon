bdeebb9176673fbfeaa988fea6185d4a06d72751
Ryan McKern <ryan.mckern@puppetlabs.com>
2017-02-07 11:27:40 -0800

    (MAINT) simplify Component spec tests
    Component is a poorly tested class, and the tests that it does have
    are somewhat repetitive and clumsy. This commit simplifies them a bit,
    and hopefully sets up adding new tests with less boilerplate.

--- spec/lib/vanagon/component_spec.rb
+++ spec/lib/vanagon/component_spec.rb
@@ -2,44 +2,41 @@ require 'vanagon/component'
 
 describe "Vanagon::Component" do
   describe "#get_environment" do
-    it "prints a deprecation warning to STDERR" do
-      comp = Vanagon::Component.new('env-test', {}, {})
-      expect { comp.get_environment }
-        .to output(/deprecated/).to_stderr
+    subject { Vanagon::Component.new('env-test', {}, {}) }
+
+    it "prints a deprecation warning to STDERR" do    
+      expect { subject.get_environment }.to output(/deprecated/).to_stderr
     end
 
     it "returns a makefile compatible environment" do
-      comp = Vanagon::Component.new('env-test', {}, {})
-      comp.environment = {'PATH' => '/usr/local/bin'}
-      expect(comp.get_environment).to eq('export PATH="/usr/local/bin"')
+      subject.environment = {'PATH' => '/usr/local/bin'}
+      expect(subject.get_environment).to eq %(export PATH="/usr/local/bin")
     end
 
     it 'merges against the existing environment' do
-      comp = Vanagon::Component.new('env-test', {}, {})
-      comp.environment = {'PATH' => '/usr/bin', 'CFLAGS' => '-I /usr/local/bin'}
-      expect(comp.get_environment).to eq('export PATH="/usr/bin" CFLAGS="-I /usr/local/bin"')
+      subject.environment = {'PATH' => '/usr/bin', 'CFLAGS' => '-I /usr/local/bin'}
+      expect(subject.get_environment).to eq %(export PATH="/usr/bin" CFLAGS="-I /usr/local/bin")
     end
 
     it 'returns : for an empty environment' do
-      comp = Vanagon::Component.new('env-test', {}, {})
-      expect(comp.get_environment).to eq(': no environment variables defined')
+      expect(subject.get_environment).to eq %(: no environment variables defined)
     end
   end
 
   describe "#get_build_dir" do
-    subject(:comp) do
+    subject do
       Vanagon::Component.new('build-dir-test', {}, {}).tap do |comp|
         comp.dirname = "build-dir-test"
       end
     end
 
     it "uses the dirname when no build_dir was set" do
-      expect(comp.get_build_dir).to eq "build-dir-test"
+      expect(subject.get_build_dir).to eq "build-dir-test"
     end
 
     it "joins the dirname and the build dir when a build_dir was set" do
-      comp.build_dir = "cmake-build"
-      expect(comp.get_build_dir).to eq File.join("build-dir-test", "cmake-build")
+      subject.build_dir = "cmake-build"
+      expect(subject.get_build_dir).to eq File.join("build-dir-test", "cmake-build")
     end
   end
 end
