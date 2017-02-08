5cebcba5539e22f38d2adb4bf0214e954dd60503
Ryan McKern <ryan.mckern@puppetlabs.com>
2017-02-07 17:13:49 -0800

    (MAINT) Add ability to enforce coverage score
    Our test coverage score has been 71.85% for a little while, so I'd
    like to set a minimum baseline for coverage at 70%. If `rake
    test:coverage` is run, it will now return a non-zero exit status if
    the coverage percentage is less than 70% (or the env. var.
    MINIMUM_SCORE).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,14 @@ if ENV["COVERAGE"]
     add_filter '.bundle'
     add_filter 'spec'
     add_filter 'vendor'
+
+    # Define a minimum coverage score, and fail if it's not met.
+    # This should probably be a Float, not an Integer but as
+    # long as it's not a String it's probably fine.
+    #
+    # The coverage score on 2017-02-07 for commit 770b67db was 71.85
+    # - Ryan McKern, 2017-02-07
+    minimum_coverage ENV['MINIMUM_SCORE'] || 70.00
   end
 end
 
