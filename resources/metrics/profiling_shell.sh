4f56ab0d225e74b66294207ee31454a8fe0b8517
Ryan McKern <ryan.mckern@puppetlabs.com>
2017-03-01 02:02:41 -0800

    (VANAGON-47) Simplify profiling_shell.sh
    Because of the platform-by-platform quirks of sending UDP messages or
    converting a decimal to an integer, `profiling_shell.sh` will now write
    run times into a batch file for each Make target it executes.
    
    Every status in the ${PIPESTATUS} array will now be checked, in an
    attempt to avoid setting `-o pipefail`, which is more strict than Make
    or Bash usually would be about failed jobs in a pipeline (say, using
    the || operator to short-circuit feeding xargs from either of two
    similar-but-different `find` commands).
    
    Simplifying `profiling_shell.sh` has also had the side-effect of
    removing the dependency on `bc`.

--- resources/metrics/profiling_shell.sh
+++ resources/metrics/profiling_shell.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-# this variant of the Profiling shell wrapper
-# uses only the Bash keyword `time`, and the generally
+# Vanagon profiling shell wrapper
+# version: 1.0.0
+# author: Ryan McKern <mckern@puppet.com>
+
+# The Vanagon Profiling shell wrapper
+# uses only the Bash keyword `time` and the generally
 # standard command `tee` to get run time. This was
 # needed because the BSD/sysv variants of `date` only
 # offer second-resolution. No milliseconds, no nano-
-# seconds.
-
-# Create copies of stdout & stderr's file descriptors
-# so that the output of the subshell that `time` runs
-# can be captured and used.
-exec 3>&1
-exec 4>&2
+# seconds. The results of `time` will be written to a
+# csv file containing a single column (times for each
+# subshell run as part of a given Make target. Each line
+# is a subshell's runtime in whole seconds with 3 decimal 
+# places of precision), named after the Make target, e.g.
+#   facter-configure.csv, facter-install.csv, etc.
+#
+# Of note is that we are intentionally avoiding using
+# `set -o pipefail` or `set -e` as these can cause
+# numerous false-positive failures depending on the
+# level of care and complexity in a given Make recipe.
 
 # Write some log files, that can be post-processed
-# relatively easily.
-__stdout="${VANAGON_TARGET}.stdout.log"
-__stderr="${VANAGON_TARGET}.stderr.log"
+# relatively easily. While you're at it, store runtimes in batches.
+if [[ -n ${VANAGON_TARGET} ]]; then
+  __batchfile="${VANAGON_TARGET}.csv"
+  __stdout="${VANAGON_TARGET}.stdout.log"
+  __stderr="${VANAGON_TARGET}.stderr.log"
+else
+  # Alternatively, if there's no defined Target, just
+  # dump that stuff in the bitbucket
+  __batchfile="/dev/null"
+  __stdout="/dev/null"
+  __stderr="/dev/null"
+fi
 
 # Define a default time format (seconds and milliseconds),
 # and then use some insane output redirection hacks to capture
@@ -24,22 +41,27 @@ __stderr="${VANAGON_TARGET}.stderr.log"
 # output to stdout & stderr but we should figure out how to
 # supress that for quieter Vanagon builds.
 TIMEFORMAT='%3R'
-__seconds="$( {
-  time bash -o pipefail "${@}" > >(tee -ai "${__stdout}" >&3) 2> >(tee -ai "${__stderr}" >&4 )
-} 2>&1)"
+{ __seconds="$( {
+    time bash "${@}" > >(tee -ai "${__stdout}" >&3) 2> >(tee -ai "${__stderr}" >&4
+  ); } 2>&1 )";
+} 3>&1 4>&2
 
-# If any part of the pipeline failed, then
-# this status should correspond to whatever
-# failing status the pipeline returned. We
-# want to check & preserve that.
-if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-  exit "${PIPESTATUS[0]}"
-fi
+# PIPESTATUS can be reset prety easily, so the value of this specific pipeline
+# should be preserved to enable correct post-processing.
+__status=("${PIPESTATUS[@]}")
 
-# Finally, if everything completed successfully,
-# finesse the numbers and fire them off to our
-# statsd interface.
-if [[ "${VANAGON_TARGET}" ]] && [[ "${VANAGON_STATSD_HOST}" ]] && [[ "${VANAGON_STATSD_PORT}" ]]; then
-  __elapsed="$(echo "scale=0;${__seconds}*1000/1" | bc -l)"
-  echo -n "vanagon.${VANAGON_PROJECT}.${VANAGON_PLATFORM}.${VANAGON_TARGET}:${__elapsed}|ms" > "/dev/udp/${VANAGON_STATSD_HOST}/${VANAGON_STATSD_PORT}"
-fi
+# allow a failure in the command pipeline to halt execution,
+# the same way Make would if the pipeline was being executed
+# directly in Bash without this wrapper. This will halt the job
+# without sending metrics for this job upstream.
+for value in "${__status[@]}"; do
+  if [[ ${value} -ne 0 ]]; then
+    exit "${value}"
+  fi
+done
+
+# Batch the runtime for this task
+echo "${__seconds}" >> "${__batchfile}"
+
+# Explicitly exit with success
+exit
