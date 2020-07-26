36f8e028b4df58c8bc672e3410bbb5c0d24d5483
Charlie Sharpsteen <chuck@puppet.com>
2020-07-26 12:39:50 -0700

    Format Docker spec tests
    This commit unifies the style of the spec tests for the Docker engine.

--- spec/lib/vanagon/engine/docker_spec.rb
+++ spec/lib/vanagon/engine/docker_spec.rb
@@ -2,20 +2,24 @@ require 'vanagon/engine/docker'
 require 'vanagon/platform'
 
 describe Vanagon::Engine::Docker do
-  let (:platform_with_docker_image) {
-    plat = Vanagon::Platform::DSL.new('debian-6-i386')
-    plat.instance_eval("platform 'debian-6-i386' do |plat|
-                      plat.docker_image 'debian-6-i386'
-                    end")
+  let (:platform_with_docker_image) do
+    plat = Vanagon::Platform::DSL.new('debian-10-amd64')
+    plat.instance_eval(<<~EOF)
+      platform 'debian-10-amd64' do |plat|
+        plat.docker_image 'debian:10-slim'
+      end
+    EOF
     plat._platform
-  }
+  end
 
-  let (:platform_without_docker_image) {
-    plat = Vanagon::Platform::DSL.new('debian-6-i386')
-    plat.instance_eval("platform 'debian-6-i386' do |plat|
-                    end")
+  let (:platform_without_docker_image) do
+    plat = Vanagon::Platform::DSL.new('debian-10-amd64')
+    plat.instance_eval(<<~EOF)
+      platform 'debian-10-amd64' do |plat|
+      end
+    EOF
     plat._platform
-  }
+  end
 
   let(:platform_with_docker_exec) do
     plat = Vanagon::Platform::DSL.new('debian-10-amd64')
@@ -34,25 +38,25 @@ describe Vanagon::Engine::Docker do
         expect(FileTest).to receive(:executable?).with(File.join(path_elem, 'docker')).and_return(false)
       end
 
-      expect { Vanagon::Engine::Docker.new(platform_with_docker_image) }.to raise_error(RuntimeError)
+      expect { described_class.new(platform_with_docker_image) }.to raise_error(RuntimeError)
     end
   end
 
   describe "#validate_platform" do
     it 'raises an error if the platform is missing a required attribute' do
       expect(Vanagon::Utilities).to receive(:find_program_on_path).with('docker').and_return('/usr/bin/docker')
-      expect { Vanagon::Engine::Docker.new(platform_without_docker_image).validate_platform }.to raise_error(Vanagon::Error)
+      expect { described_class.new(platform_without_docker_image).validate_platform }.to raise_error(Vanagon::Error)
     end
 
     it 'returns true if the platform has the required attributes' do
       expect(Vanagon::Utilities).to receive(:find_program_on_path).with('docker').and_return('/usr/bin/docker')
-      expect(Vanagon::Engine::Docker.new(platform_with_docker_image).validate_platform).to be(true)
+      expect(described_class.new(platform_with_docker_image).validate_platform).to be(true)
     end
   end
 
   it 'returns "docker" name' do
     expect(Vanagon::Utilities).to receive(:find_program_on_path).with('docker').and_return('/usr/bin/docker')
-    expect(Vanagon::Engine::Docker.new(platform_with_docker_image).name).to eq('docker')
+    expect(described_class.new(platform_with_docker_image).name).to eq('docker')
   end
 
   describe '#dispatch' do
