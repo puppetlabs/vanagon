00ed250188ce2f5f16869677e6d0f8a01144d13f
Eric Griswold <eric.griswold@puppet.com>
2020-09-11 14:50:15 -0700

    (maint) replace a tweak I made for local testing

--- lib/vanagon.rb
+++ lib/vanagon.rb
@@ -3,8 +3,7 @@ require 'time'
 LIBDIR = __dir__
 VANAGON_ROOT = File.join(__dir__, "..")
 BUILD_TIME = Time.now.iso8601
-# VANAGON_VERSION = Gem.loaded_specs["vanagon"].version.to_s
-VANAGON_VERSION = '0.0.0'
+VANAGON_VERSION = Gem.loaded_specs["vanagon"].version.to_s
 
 $:.unshift(LIBDIR) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(LIBDIR)
