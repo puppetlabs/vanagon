98e638b4853e9da7bb179f3102243e905da2d941
Jacklyn Kinsler <jacklynkinsler@Jacklyns-MacBook-Pro.local>
2020-11-19 15:38:44 -0800

    Implemented vanagon list.

--- lib/vanagon/cli/list.rb
+++ lib/vanagon/cli/list.rb
@@ -5,12 +5,13 @@ class Vanagon
     class List < Vanagon::CLI
       DOCUMENTATION = <<~DOCOPT.freeze
         Usage:
-          list [--help]
+          list [options]
 
         Options:
           -h, --help                       Display help
           -r, --projects                   Display a list of projects
           -l, --platforms                  Display a list of platforms
+          -s, --space                      Displays the list as space separated
       DOCOPT
 
       def parse(argv)
@@ -20,35 +21,32 @@ class Vanagon
         exit 1
       end
 
-      def run(_)
-        puts "hi"
-        # ENV['PROJECT_ROOT'] = Dir.pwd
+      def run(options)
+        platform_list = Dir.children('configs/platforms').map do |platform|
+          File.basename(platform, File.extname(platform))
+        end
 
-        # artifactory_warning = <<-DOC
-        #   Unable to ship packages to artifactory. Please make sure you are pointing to a
-        #   recent version of packaging in your Gemfile. Please also make sure you include
-        #   the artifactory gem in your Gemfile.
+        project_list = Dir.children('configs/projects').map do |project|
+          File.basename(project, File.extname(project))
+        end
+        
+        if options[:projects] == options[:platforms]
+          puts "- Projects", project_list, "\n", "- Platforms", platform_list, "\n"
+        elsif options[:projects]
+          puts "- Projects", project_list
+        elsif options[:platforms]
+          puts "- Platforms", platform_list
+        end
+        
+      end
 
-        #   Examples:
-        #     gem 'packaging', :github => 'puppetlabs/packaging', branch: '1.0.x'
-        #     gem 'artifactory'
-        # DOC
-
-        # if Dir['output/**/*'].select { |entry| File.file?(entry) }.empty?
-        #   warn 'vanagon: Error: No packages to ship in the "output" directory. Maybe build some first?'
-        #   exit 1
-        # end
-
-        # require 'packaging'
-        # Pkg::Util::RakeUtils.load_packaging_tasks
-        # Pkg::Util::RakeUtils.invoke_task('pl:jenkins:ship', 'artifacts', 'output')
-        # begin
-        #   Pkg::Util::RakeUtils.invoke_task('pl:jenkins:ship_to_artifactory', 'output')
-        # rescue LoadError
-        #   warn artifactory_warning
-        # rescue StandardError
-        #   warn artifactory_warning
-        # end
+      def options_translate(docopt_options)
+        translations = {
+          '--projects' => :projects,
+          '--platforms' => :platforms,
+          '--space' => :space,
+        }
+        return docopt_options.map { |k, v| [translations[k], v] }.to_h
       end
     end
   end
