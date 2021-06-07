8641ad2f533dfb55da995f30a3ec991ad49f612a
gimmy <gheorghe.popescu@puppet.com>
2021-06-07 11:05:33 +0300

    (maint) install `libarchive` on el-8
    RedHat recently updated their repos and the cmake
    version pulled is newer (3.18.2). This uses a
    libarchive method that is not available on
    3.3.2 which is the one that comes with the box.
    
    Installing a newer version of libarchive fixes the
    compliation issues with cmake 3.18.2

--- lib/vanagon/platform/defaults/el-8-aarch64.rb
+++ lib/vanagon/platform/defaults/el-8-aarch64.rb
@@ -3,7 +3,7 @@ platform "el-8-aarch64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "systemd"
 
-  packages = %w(autoconf automake createrepo gcc gcc-c++ rsync cmake make rpm-libs rpm-build)
+  packages = %w(autoconf automake createrepo gcc gcc-c++ rsync cmake make rpm-libs rpm-build libarchive)
   plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
   plat.install_build_dependencies_with "dnf install -y --allowerasing "
   plat.vmpooler_template "redhat-8-arm64"
