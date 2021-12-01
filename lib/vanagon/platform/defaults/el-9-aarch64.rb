e25aef3ba690a3e0d1b9170245f305bd409d357b
Steve Traylen <steve.traylen@cern.ch>
2021-12-01 18:36:12 +0100

    (VANAGON-174) Addition of EL-9 platform
    Add platforms to support building on el-9 based systems
    
    In CentOS Stream 9
    
    ```
    dnf install '/bin/mkdir'
    Last metadata expiration check: 0:00:09 ago on Thu 02 Dec 2021 12:25:13 PM CET.
    No match for argument: /bin/mkdir
    Error: Unable to find a match: /bin/mkdir
    ```
    
    where as
    
    ```
    dnf install '/usr/bin/mkdir'
    ```
    
    does install `coreutils-single`
    
    Note that Fedora < 26 has already been deprecated and that case can be
    ignored.

---
+++ lib/vanagon/platform/defaults/el-9-aarch64.rb
@@ -0,0 +1,10 @@
+platform "el-9-aarch64" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  packages = %w(autoconf automake createrepo gcc gcc-c++ rsync cmake make rpm-libs rpm-build libarchive)
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
+  plat.install_build_dependencies_with "dnf install -y --allowerasing "
+  plat.vmpooler_template "redhat-9-arm64"
+end
