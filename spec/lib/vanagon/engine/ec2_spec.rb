17b4f0dcff0046e7c03666cc2b2732e2b8d0dca5
Steve Traylen <steve.traylen@cern.ch>
2021-12-01 10:59:05 +0100

    (VANAGON-179) Support ruby 3 for vanagon localhost
    Allow vanagon to be run with ruby 3. In particular CentOS stream 9
    is ruby 3.0.2p107.

--- spec/lib/vanagon/engine/ec2_spec.rb
+++ spec/lib/vanagon/engine/ec2_spec.rb
@@ -29,6 +29,16 @@ if defined? ::Aws
     it 'returns "ec2" name' do
       stub_request(:get, "http://169.254.169.254/latest/meta-data/iam/security-credentials/").
           to_return(status: 200, body: "", headers: {})
+      stub_request(:put, "http://169.254.169.254/latest/api/token").
+          with(
+              headers: {
+                 'Accept'=>'*/*',
+                 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                 'User-Agent'=>'aws-sdk-ruby3/3.124.0',
+                 'X-Aws-Ec2-Metadata-Token-Ttl-Seconds'=>'21600'
+              }).
+              to_return(status: 200, body: "", headers: {})
+
       expect(Vanagon::Engine::Ec2.new(platform_ec2).name).to eq('ec2')
     end
   end
