ca56a2a61265f3ef8e4e92ab477d28875c82b7c1
Morgan Rhodes <morgan@puppet.com>
2022-04-28 14:58:09 -0700

    Revert "(maint) Add the focal-updates.list file creation and gpg key add to the provisioning step in the defaults for ubuntu-20.04-amd64 (#748)"
    This reverts commit d116e3df628cdc082f23a6e8b3a88fae0522fff6.

--- lib/vanagon/platform/defaults/ubuntu-20.04-amd64.rb
+++ lib/vanagon/platform/defaults/ubuntu-20.04-amd64.rb
@@ -4,14 +4,8 @@ platform "ubuntu-20.04-amd64" do |plat|
   plat.servicetype "systemd"
   plat.codename "focal"
 
-  # Temporary fix to add focal-updates.list repo file because it is missing from the vmpooler image
-  plat.provision_with "echo 'deb https://artifactory.delivery.puppetlabs.net/artifactory/ubuntu__remote focal-updates main restricted universe multiverse' > /etc/apt/sources.list.d/focal-updates.list;
-  echo 'deb-src https://artifactory.delivery.puppetlabs.net/artifactory/ubuntu__remote focal-updates main restricted universe multiverse' >> /etc/apt/sources.list.d/focal-updates.list"
-  
   packages = %w(build-essential devscripts make quilt pkg-config debhelper rsync fakeroot cmake)
-
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
-  plat.provision_with "curl https://apt.puppet.com/DEB-GPG-KEY-puppet-20250406 | apt-key add -"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
   plat.vmpooler_template "ubuntu-2004-x86_64"
 end
