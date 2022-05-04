34097e441bb1b16910f9c4641abf76ca516033b0
Morgan Rhodes <morgan@puppet.com>
2022-05-04 15:11:34 -0700

    (maint) Keep addition of gpg key

--- lib/vanagon/platform/defaults/ubuntu-20.04-amd64.rb
+++ lib/vanagon/platform/defaults/ubuntu-20.04-amd64.rb
@@ -6,6 +6,7 @@ platform "ubuntu-20.04-amd64" do |plat|
 
   packages = %w(build-essential devscripts make quilt pkg-config debhelper rsync fakeroot cmake)
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
+  plat.provision_with "curl https://apt.puppet.com/DEB-GPG-KEY-puppet-20250406 | apt-key add -"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
   plat.vmpooler_template "ubuntu-2004-x86_64"
 end
