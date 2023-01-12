9b2df0c436ed57a3fa166705b8e9dd7b2b3c760a
Josh Cooper <joshcooper@users.noreply.github.com>
2023-01-12 15:32:34 -0800

    (PA-4838) Update redhatfips-7-x86_64
    Push packages from puppet-runtime into vanagon defaults, see
    https://github.com/puppetlabs/puppet-runtime/blob/75fd59790e39c901a98f5820af4762d30b1a4fc3/configs/platforms/redhatfips-7-x86_64.rb
    
    Note this doesn't include pl-gcc or pl-cmake from the runtime.

--- lib/vanagon/platform/defaults/redhatfips-7-x86_64.rb
+++ lib/vanagon/platform/defaults/redhatfips-7-x86_64.rb
@@ -9,12 +9,22 @@ platform "redhatfips-7-x86_64" do |plat|
     automake
     createrepo
     gcc
+    java-1.8.0-openjdk-devel
+    libsepol
+    libsepol-devel
+    libselinux-devel
     make
+    openssl-devel
+    pkgconfig
+    readline-devel
     rpmdevtools
+    rpm-build
     rpm-libs
     rpm-sign
     rsync
+    swig
     yum-utils
+    zlib-devel
   )
   plat.provision_with "yum install --assumeyes #{packages.join(' ')}"
   plat.install_build_dependencies_with "yum install --assumeyes"
