72d592643699bb5fcce7514d60db3685b0ef843e
Josh Cooper <joshcooper@users.noreply.github.com>
2023-01-12 15:31:34 -0800

    (PA-4838) Expand for diff-ability

--- lib/vanagon/platform/defaults/redhatfips-7-x86_64.rb
+++ lib/vanagon/platform/defaults/redhatfips-7-x86_64.rb
@@ -4,7 +4,18 @@ platform "redhatfips-7-x86_64" do |plat|
   plat.servicetype "systemd"
 
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-el-7.noarch.rpm"
-  packages = %w(autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign)
+  packages = %w(
+    autoconf
+    automake
+    createrepo
+    gcc
+    make
+    rpmdevtools
+    rpm-libs
+    rpm-sign
+    rsync
+    yum-utils
+  )
   plat.provision_with "yum install --assumeyes #{packages.join(' ')}"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.vmpooler_template "redhat-fips-7-x86_64"
