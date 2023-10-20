1e4ae488bd07ee4440fddd770fc8ad013f5e0ffa
Saurabh Pandit <saurabh.pandit@perforce.com>
2023-10-20 15:48:07 +0530

    (VANAGON-231) : Added amazon linux 2023 platform for intel & arm

---
+++ lib/vanagon/platform/defaults/amazon-2023-aarch64.rb
@@ -0,0 +1,10 @@
+platform "amazon-2023-aarch64" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  packages = %w(autoconf automake createrepo gcc gcc-c++ rsync cmake make rpm-libs rpm-build libarchive)
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
+  plat.install_build_dependencies_with "dnf install -y --allowerasing "
+  plat.vmpooler_template "amazon-2023-arm64"
+end
