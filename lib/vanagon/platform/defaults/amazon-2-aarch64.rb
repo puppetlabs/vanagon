5ab7c66dde40e3d8cdcc1488e0c7c567af41dc8e
Josh Cooper <joshcooper@users.noreply.github.com>
2024-06-03 09:30:49 -0700

    Copy amazon-7 to amazon-2
    We're intentionally continuing to use the amazon-7-arm64 VM template

---
+++ lib/vanagon/platform/defaults/amazon-2-aarch64.rb
@@ -0,0 +1,10 @@
+platform "amazon-2-aarch64" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  packages = %w(autoconf automake createrepo gcc gcc-c++ rsync cmake3  make rpm-libs rpm-build libarchive)
+  plat.provision_with("yum install -y --nogpgcheck  #{packages.join(' ')}")
+  plat.install_build_dependencies_with "yum install --assumeyes"
+  plat.vmpooler_template "amazon-7-arm64"
+end
