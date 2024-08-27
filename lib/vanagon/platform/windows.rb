d12e71ef9fbd2174ae66aa02ef1ded44871174ce
Shubham Shinde <shubham.shinde@perforce.com>
2024-08-27 23:50:11 +0530

    (VANAGON-257) Enable windowsfips-2016 to use '-fips' candle and heat flags
     - Fix some rubocop complaints.
     - Some cops were moved to separate gem and renamed. Require these gems and update the names.
     - Remove unused rails cops.

--- lib/vanagon/platform/windows.rb
+++ lib/vanagon/platform/windows.rb
@@ -192,14 +192,14 @@ class Vanagon
         #   -dr             - Root DirectoryRef to point all components to
         #   -var            - Replace "SourceDir" in the @source attributes of all components with a preprocessor variable
         app_heat_flags = " -dr INSTALLDIR -v -ke -indent 2 -cg AppComponentGroup -gg -srd -t wix/filter.xslt -sreg -var var.AppSourcePath "
-        app_heat_flags += " -fips" if project.platform.name =~ /windowsfips-2012r2/
+        app_heat_flags += " -fips" if project.platform.is_windows? && project.platform.is_fips?
 
         app_source_path = "SourceDir/#{project.settings[:base_dir]}/#{project.settings[:company_id]}/#{project.settings[:product_id]}"
         # Candle.exe preprocessor vars are required due to the above double run of heat.exe, both runs of heat use
         # preprocessor variables
         candle_preprocessor = "-dAppSourcePath=\"#{app_source_path}\" "
         candle_flags = "-arch #{@architecture} #{wix_extensions}"
-        candle_flags += " -fips" if project.platform.name =~ /windowsfips-2012r2/
+        candle_flags += " -fips" if project.platform.is_windows? && project.platform.is_fips?
 
         # Enable verbose mode for the moment (will be removed for production)
         # localisation flags to be added
